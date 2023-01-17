dbd34a57765332eaf85a25a5d60746940007ce53
Yohan Caro <yohancaro7@gmail.com>
2023-01-17 18:21:20 -0500

    Material opts
    .

--- STPage/src/app/app.component.ts
+++ STPage/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { LoginDialogComponent } from './components/login-dialog/login-dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'STPage';
+  constructor(
+    private _snackBar: MatSnackBar,
+    public dialog: MatDialog) {}
+
+  openSnackBar() {
+    this._snackBar.open("Ni nosotros sabemos!", 'X');
+  }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(LoginDialogComponent, {
+
+    });
+  }
+
 }
--- STPage/src/app/app.module.ts
+++ STPage/src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@app/material.module';
+import { LoginDialogComponent } from './components/login-dialog/login-dialog.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginDialogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
